06949465425b7c00973d7bdd80d63f24d8f2ef72
youben11 <ayouben9@gmail.com>
2017-03-23 00:02:51 +0100

    packages.both customize_airootfs.sh build.sh

--- builder/airootfs/root/customize_airootfs.sh
+++ builder/airootfs/root/customize_airootfs.sh
@@ -1,13 +1,29 @@
-#!/bin/bash
+ #!/bin/bash
 
 set -e -u
 
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/#\(fr_FR\.UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/#\(ar_DZ\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
-ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+echo 'LANG="fr_FR.UTF-8"' >> /etc/locale.conf
+export LANG=fr_FR.UTF-8
 
-usermod -s /usr/bin/zsh root
+echo "KEYMAP=fr" >> /etc/vconsole.conf
+
+rm /etc/localtime
+ln -s /usr/share/zoneinfo/Africa/Algiers /etc/localtime
+
+hwclock --systohc --utc
+
+echo "ESI" > /etc/hostname
+
+useradd -m -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash utilisateur
+
+echo "utilisateur ALL=(ALL) ALL" >> /etc/sudoers
+
+usermod -s /bin/bash root
 cp -aT /etc/skel/ /root/
 chmod 700 /root
 
@@ -19,5 +35,5 @@ sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
-systemctl enable pacman-init.service choose-mirror.service
-systemctl set-default multi-user.target
+systemctl enable pacman-init.service choose-mirror.service gdm.service NetworkManager.service
+systemctl set-default graphical.target
